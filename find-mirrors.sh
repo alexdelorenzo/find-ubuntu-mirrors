8fcca0dd028a8e026cf98d7d246b3dcc4c4b418c
Alex DeLorenzo <alex@alexdelorenzo.dev>
2022-03-28 00:33:15 -0400

    Output `sources.list` entries instead of `Valid:`

--- find-mirrors.sh
+++ find-mirrors.sh
@@ -18,9 +18,13 @@ export LIST_URL="https://launchpad.net/ubuntu/+archivemirrors/"
 export SELECTOR="#mirrors_list > tbody > tr > td:nth-child(2) > a"
 export TIMEOUT=10
 
-export RC_MISSING_DEPS=1
 export NO_PARALLEL="GNU parallel is missing, installing with apt...\n"
 export NO_HTMLQ="You need to install htmlq: https://github.com/mgdm/htmlq\n"
+export APT_BIN_FMT="deb %s %s %s\n"
+export APT_SRC_FMT="deb-src %s %s %s\n"
+
+export RC_MISSING_DEPS=1
+export RC_BAD_MIRROR=2
 
 set -Eeuo pipefail
 shopt -s expand_aliases
@@ -28,6 +32,7 @@ shopt -s expand_aliases
 
 alias quiet="&>/dev/null"
 alias quiet-err="2>/dev/null"
+alias newline="printf '\n'"
 alias installHttp='python3 -m pip install --upgrade httpie'
 
 
@@ -78,12 +83,23 @@ checkRepo() {
 export -f checkRepo
 
 
+toAptSource() {
+  local url="$1"
+  local args=("$url" "$DISTRO" "$REPOSITORY")
+
+  printf "$APT_BIN_FMT" "${args[@]}"
+  printf "$APT_SRC_FMT" "${args[@]}"
+}
+export -f toAptSource
+
+
 testMirror() {
   local url="$1"
   local repoUrl="$url/$URL_PATH"
 
-  checkRepo "$repoUrl" &&
-    printf "Valid: %s\n" "$url"
+  checkRepo "$repoUrl" || return $RC_BAD_MIRROR
+  toAptSource "$url"
+  newline
 }
 export -f testMirror
 
